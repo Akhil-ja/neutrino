a909c1ba3da95474d63d3aa47c5bc7e888d9691e
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-07-12 09:09:56 +0530

    fix: Center content in Outlet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen items-center justify-center">
       <Outlet />
-    </>
+    </div>
   );
 }
 
