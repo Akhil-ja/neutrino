8e56087d48386c0b52e5256f5bd429c39961ca9b
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-07-12 12:30:13 +0530

    feat:Add configure toast to show errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,31 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { Snackbar, Alert } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import { hideError } from "./store/slices/errorSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const { message, open } = useSelector((state) => state.error);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    dispatch(hideError());
+  };
+
   return (
     <>
       <Navbar />
       <div className="flex flex-col min-h-screen p-4">
         <Outlet />
       </div>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+          {message}
+        </Alert>
+      </Snackbar>
     </>
   );
 }
